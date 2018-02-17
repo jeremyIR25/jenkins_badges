cef1f54685fe6e01f47c0a7b303e98e18fa537be
Sebastiano Miano <mianosebastiano@gmail.com>
2018-02-17 17:05:47 +0100

    Added support for test number

--- jenkins_badges/tests_badge/__init__.py
+++ jenkins_badges/tests_badge/__init__.py
@@ -1,17 +1,21 @@
 from flask import send_file, Blueprint, current_app
 import requests
 import io
+import json
+import os.path
+from jsonschema import validate, ValidationError
 from collections import namedtuple
 
 from six.moves.urllib_parse import urljoin
 
-Tests = namedtuple("Tests", ["status", "colour"])
+Tests = namedtuple("Tests", ["status", "colour", "service", "passed", "totals"])
+ServiceResults = namedtuple("ServiceResults", ["result", "passed", "failed", "total"])
 
 tests_badge = Blueprint('tests_badge', __name__)
 
-
-@tests_badge.route("/tests/<path:job_name>/<path:branch_name>", methods=['GET'])
-def send_tests_badge(job_name, branch_name):
+@tests_badge.route("/tests/<path:job_name>/<path:branch_name>", defaults={'service_name': None}, methods=['GET'])
+@tests_badge.route("/tests/<path:job_name>/<path:branch_name>/<path:service_name>", methods=['GET'])
+def send_tests_badge(job_name, branch_name, service_name):
     if job_name == "favicon.ico":
         return "", 200
 
@@ -24,7 +28,7 @@ def send_tests_badge(job_name, branch_name):
     if jresp.status_code != 200:
         return send_error_badge()
 
-    tests = extract_tests_number(jresp)
+    tests = extract_tests_number(jresp, service_name)
     surl = generate_shields_url(tests)
     sresp = requests.get(surl, stream=True)
     print("GET {} {}".format(sresp.status_code, surl))
@@ -49,11 +53,25 @@ def generate_jenkins_api_url(job_name, branch_name):
     return urljoin(current_app.config["JENKINS_BASE_URL"] + "/", api_endpoint)
 
 
-def extract_tests_number(jresp):
+def extract_tests_number(jresp, service_name):
     tests_dict = jresp.json()
     status = get_build_status(tests_dict['result'])
     colour = get_colour(status)
-    return Tests(status=status, colour=colour)
+
+    passed = 0
+    totals = 0
+    actions = tests_dict['actions']
+    for action in actions:
+    	if '_class' in action and action['_class'] == 'hudson.model.ParametersAction' and 'parameters' in action:
+    		parameters = action['parameters']
+    		for param in parameters:
+    			if '_class' in param and param['_class'] == 'hudson.model.StringParameterValue' and 'name' in param:
+    				if param['name'] == 'NUM_TESTS_SUCCEDED':
+    					passed = param['value']
+    				elif param['name'] == 'TOT_TESTS':
+    					totals = param['value']
+
+    return Tests(status=status, colour=colour, service=service_name, passed=passed, totals=totals)
 
 
 def get_build_status(result):
@@ -66,9 +84,46 @@ def get_build_status(result):
     else:
         return "unknown"
 
-def generate_shields_url(c):
+def generate_shields_url2(c):
+    if c.status == "passing" or c.status == "failure":
+        filename = current_app.config["TESTS_RESULTS_FILE_NAME"]
+        if filename and os.path.isfile(filename):
+            r = read_file_results_stats(filename, c.service)
+            if r:
+                service_colour = get_test_colour(get_build_status(r.result), r)
+                return ("https://img.shields.io/badge/tests-{0}/{1}-{2}.svg".format(r.passed, r.total, service_colour))
+
     return ("https://img.shields.io/badge/tests-{0}-{1}.svg".format(c.status, c.colour))
 
+def generate_shields_url(c):
+    if c.status == "passing" or c.status == "failure" and c.totals != 0:
+        service_colour = get_test_colour(get_build_status(c.status), c)
+        return ("https://img.shields.io/badge/tests-%20{0}%20/%20{1}%20-{2}.svg".format(c.passed, c.totals, service_colour))
+
+    return ("https://img.shields.io/badge/tests-{0}-{1}.svg".format(c.status, c.colour))
+
+def read_file_results_stats(filename, service_name):
+    with open(filename, "r", encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
+        # if not json_is_valid(json_data):
+        #    return None
+        for service in json_data['services']:
+            if service['name'] == service_name:
+                return ServiceResults(result=service['result'], passed=service['passed'], failed=service['failed'], total=service['total'])
+        # We are not asking a specific service but the total
+        return ServiceResults(result=json_data['result'], passed=json_data['passed'], failed=json_data['failed'], total=json_data['total'])
+
+def get_test_colour(status, result):
+    yellow_threshold = current_app.config['TESTS_RESULTS_YELLOW_THRESHOLD']
+    red_threshold = current_app.config['TESTS_RESULTS_YELLOW_THRESHOLD']
+
+    test_passed_ratio = round(float(result.passed) / float(result.totals), 2)
+    if test_passed_ratio > yellow_threshold:
+        return "brightgreen"
+    elif test_passed_ratio <= yellow_threshold and test_passed_ratio >= red_threshold:
+        return "yellow"
+    else:
+        return "red"
 
 def get_colour(status):
     if status == "failure":
@@ -79,3 +134,43 @@ def get_colour(status):
         return "brightgreen"
     else:
         return "lightgrey"
+
+def json_is_valid(json_data):
+    schema = {
+        "type": "object",
+        "properties": {
+            "services": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "name": {"type" : "string"},
+                        "passed": {"type" : "integer"},
+                        "failed": {"type" : "integer"},
+                        "total": {"type" : "integer"}
+                    },
+                    "required": [
+                        "name",
+                        "passed",
+                        "failed",
+                        "total"
+                    ]
+                }
+            },
+            "result": {"type" : "string"},
+            "passed": {"type" : "integer"},
+            "failed": {"type" : "integer"},
+            "total": {"type" : "integer"}
+        },
+        "required": [
+            "result",
+            "passed",
+            "failed",
+            "total"
+        ]
+    }
+    try:
+        validate(json_data, schema)
+        return True
+    except ValidationError:
+        return False
